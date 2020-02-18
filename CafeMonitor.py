9b07ff1d4f193b946a58fe09c2758b46a7667468
Ananth Ganiga <ananth.ganiga@gmail.com>
2020-02-18 21:32:57 +0530

    Two checks coded

--- CafeMonitor.py
+++ CafeMonitor.py
@@ -1,5 +1,6 @@
 import argparse
 from ExtractData import ExtractData
+import CommonUtils
 
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser()
@@ -8,4 +9,17 @@ if __name__ == '__main__' :
     args = parser.parse_args()
     ed = ExtractData(args.SheetName, args.Range)
     Last30Days = ed.downloadData()
-    
+    prevBD = CommonUtils.__getDate(-3)
+    print(prevBD)
+    try:
+        prevDayDict = Last30Days[prevBD]
+    except KeyError:
+        CommonUtils.__logging("Sheet not updated for {}".format(prevBD))
+
+    cashTans = prevDayDict['CashAtTheEnd']
+    nonCashTrans = prevDayDict['CardPaytm'] + prevDayDict['Swiggy']
+    expense = prevDayDict['CashExpenses']
+    difference = prevDayDict['TotalSale'] - ( cashTans + nonCashTrans) - expense
+    CommonUtils.__logging("Amount Differing for {} is Rs.{}.00".format(prevBD, int(difference)))
+
+
---
+++ CommonUtils.py
@@ -0,0 +1,17 @@
+import datetime
+
+def __getDate(dayMinus) :
+    '''
+    :param dayMinus: No. of day to be subtracted
+    :returns String formatted dd/mm/yyyy:
+    '''
+    dayMinus *= -1
+    #### Getting the previous day #################
+    today = datetime.datetime.today()
+    previousDay = today - datetime.timedelta(dayMinus)
+    prevBD = previousDay.strftime("%d/%m/%Y")
+    return prevBD
+#################################################
+
+def __logging(msg) :
+    print(msg)
